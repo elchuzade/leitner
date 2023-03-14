98cf6dffcd92d58ce62cb98ed83228ea9b54d89d
Kamran Elchuzade <elchuzade@gmail.com>
2023-03-15 00:04:05 +0100

    removed redundant bottom navigation scss

--- client/src/components/signature/Signature.tsx
+++ client/src/components/signature/Signature.tsx
@@ -3,8 +3,13 @@ import React from "react";
 const Signature = () => {
   return (
     <div className="wrapper-signature">
-      <a href="https://elchuzade.com" target="_blank" className="signature">
-        <i>Kamran Elchuzade</i>
+      <a
+        href="https://elchuzade.com"
+        target="_blank"
+        rel="noreferrer"
+        className="signature"
+      >
+        Kamran Elchuzade
       </a>
     </div>
   );
